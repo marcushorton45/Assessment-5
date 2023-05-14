c6fe1be5875936c3baa0e50835bfbd1e43ac8230
Marcus Horton <marcushorton45@gmal.com>
2023-05-14 15:27:14 -0600

    done

---
+++ data-modeling.sql
@@ -0,0 +1,25 @@
+-- animals
+- animal_id
+- name
+- age
+- gender
+
+-- species
+- species_id
+- animal_id (one to many)
+- species 
+
+-- favorite_activity
+- activity_id
+- animal_id (one to many)
+- favorite_activity
+
+-- favorite_toy
+- toy_id
+- animal_id (one to many)
+- favorite_toy
+
+-- color
+- color_id
+- animal_id (one to many)
+- color
